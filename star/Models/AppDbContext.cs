b8b373e952390afd956eed6240c20d66fd3faab4
hashiyuta12 <hashimoto.3129@gmail.com>
2023-06-26 23:11:41 +0900

    first commit

---
+++ star/Models/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace star.Models
+{
+    public class AppDbContext : DbContext
+    {
+        
+        public AppDbContext()
+        {
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public virtual DbSet<Datas> Datas { get; set; }
+
+        private static readonly IConfigurationRoot _configBuilder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(path: "appsettings.json")
+            .Build();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            ServerVersion serverVersion = ServerVersion.Parse(_configBuilder["Data:ServerVersion"]);
+            string connectionString = _configBuilder["Data:ConnectionString"];
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+    }
+}
---
+++ star/Models/Datas.cs
@@ -0,0 +1,11 @@
+﻿namespace star.Models
+{
+    public partial class Datas
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Price { get; set; }
+        public string Publisher { get; set; }
+    }
+}
